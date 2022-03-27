2431817bc7f9c79fa90462a0de95af06f93d52f9
Ivan Y <ivany@subetuweb.site>
2022-03-27 13:27:21 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/yes/japan/yes_japan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yes.japan.yes_japan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Yes Japan!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Text(
+              "\u3042",                             //'い 木 山',0xE3
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 100,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
